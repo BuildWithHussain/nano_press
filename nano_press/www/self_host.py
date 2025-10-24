701e531c0a2b14ff8108e9c1e8e413186df1ac99
M Venkatesh <venkateshvenki404224@gmail.com>
2025-10-24 18:14:56 +0000

    feat: fetch apps from the backend and additional fileds in apps doctype

--- nano_press/www/self_host.py
+++ nano_press/www/self_host.py
@@ -5,4 +5,12 @@ from nano_press.nano_press.doctype.server.server import Server
 
 def get_context(context):
 	context.public_key = Server._read_local_public_key()
+	context.apps = get_apps()
 	return context
+
+
+def get_apps():
+	apps = frappe.db.get_list(
+		"Apps", filters={"is_public": 1, "enabled": 1}, fields=["name", "branch", "repo_url", "scrubbed_name"]
+	)
+	return apps
