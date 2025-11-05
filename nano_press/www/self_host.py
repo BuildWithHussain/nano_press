2f07959fa7b924cbeba85a4d463ec81d19ce6602
M Venkatesh <venkateshvenki404224@gmail.com>
2025-11-05 16:16:36 +0000

    feat: enable Traefik deployment with user email

--- nano_press/www/self_host.py
+++ nano_press/www/self_host.py
@@ -6,6 +6,7 @@ from nano_press.nano_press.doctype.server.server import Server
 def get_context(context):
 	context.public_key = Server._read_local_public_key()
 	context.apps = get_apps()
+	context.user_email = frappe.session.user
 	return context
 
 
