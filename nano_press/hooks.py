2c992a89148a082d95e7dde6f93cdbabd1f0aec3
M Venkatesh <venkateshvenki404224@gmail.com>
2025-12-07 06:52:43 +0000

    wip: implement landing page and sidebar UI
    Add public landing page and PenguinUI sidebar navigation.
    Still needs browser cache clearing and layout fixes.
    
    Partial implementation of #19

--- nano_press/hooks.py
+++ nano_press/hooks.py
@@ -68,7 +68,7 @@ fixtures = [
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+home_page = "index"
 
 # website user home page (by Role)
 # role_home_page = {
