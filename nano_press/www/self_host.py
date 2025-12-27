5e5c5dabe4e5d9233086280c04f1e74622aeb6e5
M Venkatesh <venkateshvenki404224@gmail.com>
2025-12-27 10:19:09 +0000

    feat: populate old server list when its already available.

--- nano_press/www/self_host.py
+++ nano_press/www/self_host.py
@@ -7,7 +7,17 @@ def get_context(context):
 	context.public_key = Server._read_local_public_key()
 	context.apps = get_apps()
 	context.user_email = frappe.session.user
+	context.user_servers = get_user_servers()
 	return context
+
+
+def get_user_servers():
+	return frappe.db.get_list(
+		"Server",
+		filters={"owner": frappe.session.user},
+		fields=["name", "server_ip", "ssh_user", "ssh_port", "verify_status"],
+		order_by="creation desc",
+	)
 
 
 def get_apps():
