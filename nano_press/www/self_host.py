faef411d5a2ecb998cc5dbf7b9a081db2273a1bb
M Venkatesh <venkateshvenki404224@gmail.com>
2026-01-22 18:37:49 +0000

    fix(ui): prevent dialog flash and use consistent Frappe ORM methods
    apps.html:
    - Add x-cloak to custom app dialog to prevent flash on page load
    - Fixes FOUC (Flash of Unstyled Content) where dialog appeared briefly before Alpine.js initialized
    
    self_host.py:
    - Replace frappe.db.get_list() with frappe.get_all() in get_user_servers() and get_apps()
    - Replace frappe.db.get_all() with frappe.get_all() in get_frappe_versions()
    - All three functions now consistently use frappe.get_all() following Frappe ORM best practices

--- nano_press/www/self_host.py
+++ nano_press/www/self_host.py
@@ -7,11 +7,31 @@ def get_context(context):
 	context.public_key = Server._read_local_public_key()
 	context.apps = get_apps()
 	context.user_email = frappe.session.user
+	context.user_servers = get_user_servers()
+	context.frappe_versions = get_frappe_versions()
 	return context
 
 
-def get_apps():
-	apps = frappe.db.get_list(
-		"Apps", filters={"is_public": 1, "enabled": 1}, fields=["name", "branch", "repo_url", "scrubbed_name"]
+def get_user_servers():
+	return frappe.get_all(
+		"Server",
+		filters={"owner": frappe.session.user},
+		fields=["name", "server_ip", "ssh_user", "ssh_port", "verify_status"],
+		order_by="creation desc",
 	)
-	return apps
+
+
+def get_apps():
+	return frappe.get_all(
+		"Apps",
+		filters={"is_public": 1, "enabled": 1},
+		fields=["name", "branch", "repo_url", "scrubbed_name", "frappe", "app_logo"],
+	)
+
+
+def get_frappe_versions():
+	return frappe.get_all(
+		"App Version",
+		fields=["version", "scrubbed_version"],
+		order_by="creation asc",
+	)
